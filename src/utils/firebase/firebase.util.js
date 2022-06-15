4faefdcdcc231646872c0fdfa8a645a22f8a4c26
Masud28-Tech <masud.tech28@gmail.com>
2022-06-15 18:04:34 +0530

    Commit 04: Done with firebase-util setup for : 1. User Auth, 2. Create UserDocument in firebase database

--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app"; // importing the (main thing that is needed to start using firebase auth/db/etc service) firebase app from firebase
 
-import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider} from "firebase/auth";
+import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider} from "firebase/auth"; // Authentication imports from firebase
+
+import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"; // Database imports from firebase
  
 
 // Your web app's Firebase configuration
@@ -13,9 +15,10 @@ const firebaseConfig = {
   appId: "1:74407950256:web:5b8c731d134e7ffed20875"
 };
 
-// Initialize Firebase
+// INITIALIZATION OF FIREBASE APP
 const firebaseApp = initializeApp(firebaseConfig);
 
+// AUTHENTICATION PART
 // authentication/permission from Google that a user exits or signed in before (like a confirmation by firebase from Google about user's identity)
 const provider = new GoogleAuthProvider();
 
@@ -25,3 +28,36 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider); // for create function (signInWithGooglePopup) that opens a pop-up for sign in using gmail
+
+
+
+// DATABASE PART
+export const db = getFirestore();
+
+// Creating user document(Table) in firebase database
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid); // 'doc' method of firestore to create document & needs {db(database instance), collection name(table name), unique id/key/identifier}
+    // console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef); // 'getDoc' methode creates a Snapshot of collection/document reference created by 'doc' method, and further can be used...
+    // console.log(userSnapshot.exists());
+
+    // NOW CREATING/SAVING USER DOCUMENT IN THE DATABASE USING "setDoc" METHODE
+
+    // Check: That a user already exist or not in the db
+    // If doesn't exits then create it.
+    if(!userSnapshot.exists()){
+      
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await setDoc(userDocRef, {displayName, email, createdAt}); // Main methode to create/save user document
+      }
+      catch(error){
+        console.log("Error creating the user", error.message);
+      }
+    }
+    // If exist then return 'userDocRef'
+    return userDocRef;
+}
