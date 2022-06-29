62da246ced76f1041398cb350593a53db2b54d31
Masud28-Tech <masud.tech28@gmail.com>
2022-06-29 14:34:52 +0530

    Commit 10.5 : Final fixes in 'Directory' page/component such as => navigation to the respective routes on clicking the direcotry block in home page.

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 import './directory-item.styles.scss';
 
 const DirectoryItem = ({ directory }) => {
-    const { imageUrl, title, size } = directory;
+    const { imageUrl, title, size, route } = directory;
+
+    const navigate = useNavigate();
+
+    const navigationHandler = () => navigate(route);
+
     return (
-        <div className={`${size} directory-item-container`}>
+        <div className={`${size} directory-item-container`} onClick={navigationHandler}>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className="directory-item-body">
                 <h2>{title}</h2>
